bb675b62fed9a17d19620287212c42d4ee07767a
Muhammed Semih <semihmuhammedekmen@gmail.com>
2024-05-13 15:33:52 +0300

    change

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,14 @@
-// Select all navbar links
-const navLinks = document.querySelectorAll(".navbar a");
-
-// Function to handle click event
 function handleClick(event) {
-  // Prevent default link behavior
   event.preventDefault();
 
-  // Get the ID of the section associated with the clicked link
   const sectionId = this.getAttribute("href").substring(1);
 
-  // Log the ID of the section to the console
-  console.log(`Tiklanan Section ID'si ${sectionId}`);
-}
+  console.log(`Clicked on ${sectionId}`);
 
-// Attach click event listener to each navbar link
-navLinks.forEach((link) => {
-  link.addEventListener("click", handleClick);
-});
+  setTimeout(() => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  }, 100);
+}
