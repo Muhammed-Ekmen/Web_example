180cc9b79cecf8a6c8b86cc4116c172c501ccbde
Muhammed Semih <semihmuhammedekmen@gmail.com>
2024-05-29 14:30:25 +0300

    Odev 3

--- javascript/main.js
+++ javascript/main.js
@@ -15,37 +15,37 @@ function handleClick(event) {
 
 // main.js
 
-import ProductFetcher from './ProductFetcher.js';
+import ProductFetcher from "./ProductFetcher.js";
 
-ProductFetcher.fetchProducts()
-  .then((data) => {
-    const productTableBody = document.querySelector("#product-table tbody");
-    // Loop through the array of products and create table rows to display their details
-    data.forEach((product) => {
-      const row = document.createElement("tr");
-      // Create table cells for image, title, price, and description
-      const imageCell = document.createElement("td");
-      const titleCell = document.createElement("td");
-      const priceCell = document.createElement("td");
-      const descriptionCell = document.createElement("td");
-      // Create image element and set its attributes
-      const image = document.createElement("img");
-      image.width = 250
-      image.height = 250
-      image.src = product.image;
-      image.alt = product.title;
-      // Set other cell content
-      titleCell.textContent = product.title;
-      priceCell.textContent = "$" + product.price;
-      descriptionCell.textContent = product.description;
-      // Append cells to the row
-      imageCell.appendChild(image);
-      row.appendChild(imageCell);
-      row.appendChild(titleCell);
-      row.appendChild(priceCell);
-      row.appendChild(descriptionCell);
-      // Append row to the table body
-      productTableBody.appendChild(row);
-    });
+ProductFetcher.fetchProducts().then((data) => {
+  console.log("fetch");
+  console.log(data);
+  const productTableBody = document.querySelector("#product-table tbody");
+  // Loop through the array of products and create table rows to display their details
+  data.forEach((product) => {
+    const row = document.createElement("tr");
+    // Create table cells for image, title, price, and description
+    const imageCell = document.createElement("td");
+    const titleCell = document.createElement("td");
+    const priceCell = document.createElement("td");
+    const descriptionCell = document.createElement("td");
+    // Create image element and set its attributes
+    const image = document.createElement("img");
+    image.width = 250;
+    image.height = 250;
+    image.src = product.image;
+    image.alt = product.title;
+    // Set other cell content
+    titleCell.textContent = product.title;
+    priceCell.textContent = "$" + product.price;
+    descriptionCell.textContent = product.description;
+    // Append cells to the row
+    imageCell.appendChild(image);
+    row.appendChild(imageCell);
+    row.appendChild(titleCell);
+    row.appendChild(priceCell);
+    row.appendChild(descriptionCell);
+    // Append row to the table body
+    productTableBody.appendChild(row);
   });
-
+});
